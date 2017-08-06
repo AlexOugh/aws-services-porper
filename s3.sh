ba150bb85d956aa0a049d8d91a908c634ebb257b
Alex Ough <alex.ough@sungardas.com>
2017-08-05 22:29:25 -0500

    merged swagger.yaml with template.yaml

--- s3.sh
+++ s3.sh
@@ -1,14 +1,40 @@
 #!/bin/sh
 
-# Currently, the swagger.yaml does NOT support to set AWS Account Id and Region dynamically
-# so creating a stack in a different accounts/regions from where this template and artifacts are shared is failing
-# until then, do NOT set "S3_SHARED_BUCKET_NAME"
-if [ "$S3_SHARED_BUCKET_NAME" != "" ];
-then
-  # make lambda zip file public
-  aws s3 cp `cat samTemplate.yaml | shyaml get-value Resources.ProxyFunction.Properties.CodeUri` s3://$S3_SHARED_BUCKET_NAME  --acl public-read
-  # make swagger file public
-  aws s3 cp `cat samTemplate.yaml | shyaml get-value Resources.ApiGatewayApi.Properties.DefinitionUri` s3://$S3_SHARED_BUCKET_NAME  --acl public-read
-  # upload template file
-  aws s3 cp ./samTemplate.yaml s3://$S3_SHARED_BUCKET_NAME/samTemplate.yaml --acl public-read
-fi
+# *** Prerequisite:
+# The bucket specified in "S3_SHARED_BUCKET_NAME" must allow the account that copies its artifact by specifying this policy in the bucket's Bucket Policy
+# {
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Sid": "111",
+#            "Effect": "Allow",
+#            "Principal": {
+#                "AWS": "arn:aws:iam::<Account_Id_that_copies_its_artifact>:root"
+#            },
+#            "Action": "s3:PutObject",
+#            "Resource": "arn:aws:s3:::<$S3_SHARED_BUCKET_NAME>/*"
+#        }
+#    ]
+# }
+
+
+# Needs 2 steps to execute commands Manually
+# 1. Add "S3_SHARED_BUCKET_NAME" in "Environment variables" of CodeBuild project
+# 2. Add below policy in the CodeBuild's role policy document
+# {
+#   "Action": [
+#     "s3:*"
+#   ],
+#   "Resource": [
+#     "arn:aws:s3:::<$S3_SHARED_BUCKET_NAME>/*"
+#   ],
+#   "Effect": "Allow"
+# }
+
+# OR use "codepipeline_with_shared_bucket.yaml" cloudformation when building its pipeline stack
+
+
+# make lambda zip file public
+aws s3 cp `cat samTemplate.yaml | shyaml get-value Resources.ProxyFunction.Properties.CodeUri` s3://$S3_SHARED_BUCKET_NAME  --acl public-read
+# upload template file
+aws s3 cp ./samTemplate.yaml s3://$S3_SHARED_BUCKET_NAME/samTemplate.yaml --acl public-read
