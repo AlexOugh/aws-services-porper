79baa9f04ea79f6ed33a8aec024156ab6cfcf22a
Alex Ough <alex.ough@sungardas.com>
2019-04-17 22:00:46 -0500

    basestring -> str

--- src/api_handler.py
+++ src/api_handler.py
@@ -44,12 +44,12 @@ def lambda_handler(event, context):
     query_params = event.get('queryStringParameters')
     if not query_params:
         query_params = {}
-    elif isinstance(query_params, basestring):
+    elif isinstance(query_params, str):
         query_params = json.loads(query_params)
     post_data = event.get('body')
     if not post_data:
         post_data = {}
-    elif isinstance(post_data, basestring):
+    elif isinstance(post_data, str):
         post_data = json.loads(post_data)
     params = post_data;
     if method == 'get':
