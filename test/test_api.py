56b974a9ee7dba3289cc807826096a2d17d9a5d2
Gregory Cox <gregcox11@gmail.com>
2019-06-05 14:52:30 -0500

    added tests

--- test/test_api.py
+++ test/test_api.py
@@ -21,6 +21,11 @@ Host = os.environ.get('PORPER_ENDPOINT')
 authToken = os.environ.get('AUTH_TOKEN')
 region = os.environ.get('AWS_DEFAULT_REGION')
 
+test_group = {
+            "customer_id": "64b3bbf4-1f3d-446f-b697-06c2a68f2053",
+            "name": "Testing321"
+}
+
 
 def genUrl(Host, Branch, Path):
     return "https://{}/{}{}".format(Host, Branch, Path)
@@ -87,6 +92,56 @@ class ProxyUserTest(unittest.TestCase):
         pp(ret)
         assert len(ret['body']) > 0 and type(ret['body']) == list
 
+class ProxyGroupTest(unittest.TestCase):
+    def __init__(self, testName, env):
+        super(ProxyGroupTest, self).__init__(testName)
+        self.env = env
+
+    def test_group_post(self):
+        ret = run_lambda(
+            path=f"/group",
+            method="post",
+            body=json.dumps(test_group),
+            env=self.env
+        )
+        from pprint import pprint as pp
+        print("response====================")
+        pp(ret)
+        try:
+            id = ret["body"]["id"]
+        except:
+            id = ret["body"]
+        run_lambda(method="delete", path=f'/group', body=json.dumps({"id": id}), env=self.env)
+        assert ret["statusCode"] == 200
+        assert ret["body"]["name"] == "Testing321"
+
+    def test_group_put(self):
+        ret = run_lambda(
+            path=f"/group",
+            method="post",
+            body=json.dumps(test_group),
+            env=self.env
+        )
+
+        from pprint import pprint as pp
+        print("response====================")
+        pp(ret)
+        try:
+            id = ret["body"]["id"]
+        except:
+            id = ret["body"]
+
+        ret = run_lambda(
+            path=f"/group",
+            method="put",
+            body=json.dumps({"id": id, "name": "Testing654321"}),
+            env=self.env
+        )
+
+        run_lambda(method="delete", path=f'/group', body=json.dumps({"id": id}), env=self.env)
+        assert ret["statusCode"] == 200
+        assert ret["body"]["name"] == "Testing654321"
+
 
 
 
@@ -102,7 +157,12 @@ if __name__ == '__main__':
 
     suite = unittest.TestSuite()
 
+
     suite.addTest(ProxyUserTest("test_user_get", env))
+
+
+    suite.addTest(ProxyGroupTest("test_group_post", env))
+    suite.addTest(ProxyGroupTest("test_group_put", env))
 
     test_runner = unittest.TextTestRunner(verbosity=2, resultclass=unittest.TextTestResult) # .run(suite)
     result = test_runner.run(suite)
